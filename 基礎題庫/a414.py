ce8b9c123c25418cbeed5a8ad5e4f870b50c2376
yimang <lucasbbtung@gmail.com>
2024-07-06 16:51:14 +0800

    Update a414.py

--- 基礎題庫/a414.py
+++ 基礎題庫/a414.py
@@ -5,13 +5,8 @@ for line in sys.stdin:
     if line == "0":
         break
     try:
-        a = bin(int(line) + 1)[2:][::-1]
-        b = 0
-        for char in a:
-            if char == "1":
-                break
-            b += 1
-        print(b)
+        a = [i for i in bin(int(line) + 1)[2:][::-1]]
+        print(a.index("1"))
     except ValueError:
         continue
 #就是將整個字串反轉，然後計算字串到第幾位會從0變成1，就是他要的答案了
